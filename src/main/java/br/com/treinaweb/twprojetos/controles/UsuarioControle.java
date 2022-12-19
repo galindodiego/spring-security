1a108dae43c0bf6688f3d76f6859c98d5114a90f
Diego Galindo <diegogalindo84@gmail.com>
2022-12-19 15:58:01 -0300

    Alteração de senha de login implementada

--- src/main/java/br/com/treinaweb/twprojetos/controles/UsuarioControle.java
+++ src/main/java/br/com/treinaweb/twprojetos/controles/UsuarioControle.java
@@ -1,13 +1,50 @@
 package br.com.treinaweb.twprojetos.controles;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.twprojetos.dao.AlterarSenhaDAO;
+import br.com.treinaweb.twprojetos.entidades.Funcionario;
+import br.com.treinaweb.twprojetos.repositorios.FuncionarioRepositorio;
+import br.com.treinaweb.twprojetos.utils.SenhaUtils;
 
 @Controller
 public class UsuarioControle {
+
+    @Autowired
+    private FuncionarioRepositorio funcionarioRepositorio;
     
     @GetMapping("/login")
     public String login(){
         return "usuario/login";
+    }
+
+    @GetMapping("/perfil")
+    public ModelAndView perfil(Principal principal){
+        ModelAndView modelAndView = new ModelAndView("usuario/perfil");
+        Funcionario usuario = funcionarioRepositorio.findByEmail(principal.getName()).get();
+        modelAndView.addObject("usuario", usuario);
+        modelAndView.addObject("alterarSenhaForm", new AlterarSenhaDAO());
+
+        return modelAndView;
+    }
+
+    @PostMapping("/alterar-senha")
+    public String alterarSenha(AlterarSenhaDAO form, Principal principal){
+        Funcionario usuario = funcionarioRepositorio.findByEmail(principal.getName()).get();
+
+        if(SenhaUtils.matches(form.getSenhaAtual(), usuario.getSenha())){
+            usuario.setSenha(SenhaUtils.encode(form.getNovaSenha()));
+
+            funcionarioRepositorio.save(usuario);
+        }
+
+        return "redirect:/perfil";
+
     }
 }
